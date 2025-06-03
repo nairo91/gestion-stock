a98ab75540dc48ae16e0e57e4e8679b0464b4657
Orian Mirona <orian.mirona@gmail.com>
2025-06-03 12:08:58 +0200

    nouveau test emp

--- models/Emplacement.js
+++ models/Emplacement.js
@@ -1,39 +1,50 @@
-// models/Emplacement.js
-module.exports = (sequelize, DataTypes) => {
-  const Emplacement = sequelize.define('Emplacement', {
-    nom: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.STRING
-    },
-    chantierId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    parentId: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    }
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Emplacement = sequelize.define('Emplacement', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  chantierId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  parentId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+}, {
+  tableName: 'emplacements',
+  timestamps: true,
+});
+
+Emplacement.associate = function (models) {
+  Emplacement.belongsTo(models.Chantier, {
+    foreignKey: 'chantierId',
+    as: 'chantier',
+    onDelete: 'CASCADE',
   });
 
-  Emplacement.associate = function(models) {
-    Emplacement.belongsTo(models.Chantier, {
-      foreignKey: 'chantierId',
-      as: 'chantier'
-    });
+  Emplacement.belongsTo(models.Emplacement, {
+    foreignKey: 'parentId',
+    as: 'parent',
+    onDelete: 'SET NULL',
+  });
 
-    Emplacement.belongsTo(models.Emplacement, {
-      foreignKey: 'parentId',
-      as: 'parent'
-    });
-
-    Emplacement.hasMany(models.Emplacement, {
-      foreignKey: 'parentId',
-      as: 'enfants'
-    });
-  };
-
-  return Emplacement;
+  Emplacement.hasMany(models.Emplacement, {
+    foreignKey: 'parentId',
+    as: 'sousEmplacements',
+  });
 };
+
+module.exports = Emplacement;
