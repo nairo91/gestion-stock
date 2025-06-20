8b14a1287515477ccc5d1f3a97f28bcc4ecd0ecb
nairo91 <101805586+nairo91@users.noreply.github.com>
2025-06-20 12:47:13 +0200

    Fix image upload path

--- routes/vehicule.js
+++ routes/vehicule.js
@@ -28,7 +28,7 @@ function checkAdmin(req, res, next) {
 // Configuration Multer pour les uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, path.join(__dirname, '..', 'uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
